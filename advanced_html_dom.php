35a5a3337249b3f80e82ef56fac9abec939e7a3a
netanswer-devs <46164985+netanswer-devs@users.noreply.github.com>
2019-03-26 11:20:12 +0100

    Update advanced_html_dom.php

--- advanced_html_dom.php
+++ advanced_html_dom.php
@@ -800,19 +800,19 @@ class CSS{
   public function to_s(){ return $this->text; }
 }
 
-if (!function_exists('str_get_html') {
+if (!function_exists('str_get_html')) {
 	function str_get_html($html){ return new AdvancedHtmlDom($html); }
 }
 
-if (!function_exists('file_get_html') {
+if (!function_exists('file_get_html')) {
 	function file_get_html($url){ return str_get_html(file_get_contents($url)); }
 }
 
-if (!function_exists('str_get_xml') {
+if (!function_exists('str_get_xml')) {
 	function str_get_xml($html){ return new AdvancedHtmlDom($html, true); }
 }
     
-if (!function_exists('file_get_xml') {
+if (!function_exists('file_get_xml')) {
 	function file_get_xml($url){ return str_get_xml(file_get_contents($url)); }
 }
 }
